6e602308fa326e3a1322c3f52719751f891e0733
James Lim <james.lim.ori@gmail.com>
2020-07-19 05:02:40 +1000

    added color changes

--- config.def.h
+++ config.def.h
@@ -10,9 +10,9 @@ static const char *fonts[] = {
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+                /*     fg         bg       */
+	[SchemeNorm] = { "#bbbbbb", "#231a18" },
+	[SchemeSel] = { "#eeeeee", "#BA6E5B" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
